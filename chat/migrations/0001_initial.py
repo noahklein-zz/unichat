bb23bf73c0666922a8b56402050b6550a36ee912
Noah Klein <noahklein94@gmail.com>
2015-09-10 23:48:33 -0400

    created Chatroom and Message models

---
+++ chat/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Chatroom',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=32)),
+                ('occupants', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=32)),
+                ('text', models.CharField(max_length=512)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('chatroom', models.ForeignKey(to='chat.Chatroom')),
+            ],
+        ),
+    ]
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Chatroom(models.Model):
+    name = models.CharField(max_length=32)
+    occupants = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class Message(models.Model):
+    chatroom = models.ForeignKey(Chatroom)
+    username = models.CharField(max_length=32)
+    text = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.text
